d2d3d0270cb7da329ccf43d82382f72b74296a1d
Kv.se7en <shaoxinheng@gmail.com>
2012-06-29 16:41:41 +0800

    fix bugs

--- old/v3gp/contentscript.js
+++ old/v3gp/contentscript.js
@@ -10,38 +10,41 @@
             this.removeTarget();
         },
         changeDownloadBtn:function(){
-            
-            $("a img").each(function(){
-                // find the download buttons
-                if( $(this).attr("src").indexOf("down.png") > 0){
-                    var
-                         tag_a = $(this).parent(),
-                         // the original href
-                         original_href = $(this).parent().attr("href"),
-                    
-                        // get info from original href
-                         info = original_href.slice(original_href.indexOf("(")+1,original_href.indexOf(";")),
-                         data = info.split("','"),
+            setTimeout(function(){
+                $("a img").each(function(){
+                    // find the download buttons
+                    if( $(this).attr("src").indexOf("down.png") > 0){
+                        var
+                             tag_a = $(this).parent(),
+                             // the original href
+                             original_href = $(this).parent().attr("href"),
 
-                        // property rid : used for setting cookie
-                         rid = $.trim(data[0].slice(1,data[0].length)),
+                            // get info from original href
+                             info = original_href.slice(original_href.indexOf("(")+1,original_href.indexOf(";")),
+                             data = info.split("','"),
 
-                        // aid & jtitle used for download link
-                         aid = $.trim(data[1]),
-                         jtitle = $.trim(data[2].slice(0,data[2].length-2)),
-                         new_href;
-                    
-                    $.ajax({
-                        url: "http://360-v3.v3gp.com/play.php?file="+aid,
-                        success:function(data){
-                            var data = $.parseXML(data);
-                            var url = $('track',data).attr('url');
-                            new_href = url + "#name=" + encodeURIComponent(jtitle) + '&content-type=' + encodeURIComponent('audio/mp3');
-                            tag_a.removeAttr("href").attr("rel","download").attr('href',new_href);
-                        }
-                    });
-                }
-            });
+                            // property rid : used for setting cookie
+                             rid = $.trim(data[0].slice(1,data[0].length)),
+
+                            // aid & jtitle used for download link
+                             aid = $.trim(data[1]),
+                             jtitle = $.trim(data[2].slice(0,data[2].length-2)),
+                             new_href;
+
+                        $.ajax({
+                            url: "http://360-v3.v3gp.com/play.php?file="+aid,
+                            success:function(data){
+                                var data = $.parseXML(data);
+                                var url = $('track',data).attr('url');
+                                console.log(url);
+                                new_href = url + "#name=" + encodeURIComponent(jtitle) + '&content-type=' + encodeURIComponent('audio/mp3');
+                                tag_a.removeAttr("href").attr("rel","download").attr('href',new_href);
+                            }
+                        });
+                    }
+                });
+            },500);
+
 
 
         },

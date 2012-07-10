441b8b220151be7f764e31ce46b5fd17b5ca91bf
InMyWnd <shaoxinheng@gmail.com>
2012-07-11 00:34:38 +0800

    Fancy

--- fancy_plugins/video_detect/contentscript.js
+++ fancy_plugins/video_detect/contentscript.js
@@ -1,1 +1,35 @@
-
+(function($){
+    var wanDouJiaExt = {
+        init: function(){
+            wanDouJiaExt.modifyDetailPage();
+            wanDouJiaExt.removeTarget();
+        },
+        modifyDetailPage:function(){
+            var name = $("#subtitle").text(),
+                page_url = location.href;
+            if( name !=null ){
+                chrome.extension.sendMessage({name: name,page_url:page_url}, function(response) {
+                    console.log(response);
+                });
+            }
+        },
+        // get params from a url
+        request_url:function (url){
+            var url = url,
+                request = new Object(),
+                strs;
+            if (url.indexOf("?") != -1) {
+                var str = url.substr(url.indexOf("?")+1,url.length-url.indexOf("?"));
+                strs = str.split("&");
+                for(var i = 0; i < strs.length; i ++) {
+                    request[strs[i].split("=")[0]] = strs[i].split("=")[1];
+                }
+            }
+            return request;
+        },
+        removeTarget:function(){
+            $('a','body').removeAttr('target');
+        }
+    };
+    wanDouJiaExt.init();
+})(jQuery);

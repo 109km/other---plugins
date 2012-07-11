2f97b450e96f1340b2303f88f2a5fc12600fde22
InMyWnd <shaoxinheng@gmail.com>
2012-07-12 00:27:22 +0800

    Detect

--- fancy_plugins/video_detect/background.js
+++ fancy_plugins/video_detect/background.js
@@ -1,64 +1,7 @@
 console.log("Background is running.");
 
+// 存储视频信息
 var videos = {};
-/*
-if( localStorage.length > 0 ){
-    for( var i=0; i < localStorage.length ; i++ ){
-        var key = localStorage.key(i);
-        videos[key] = localStorage.getItem(key);
-    }
-}
-*/
-
-// 加入此视频的
-var i = 0,
-    is_video_page = false;
-
-chrome.extension.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        videos[i] = {};
-        videos[i]["name"] = request.name;
-        videos[i]["page_url"] = request.page_url;
-        sendResponse(videos);
-        is_video_page = true;
-    }
-);
-
-
-// detect received objects
-chrome.webRequest.onResponseStarted.addListener(
-    function(details){
-        //筛选符合条件的details
-        if( details.type == "object" && details.frameId == "0" && is_video_page){
-            var headers = details.responseHeaders,
-                is_video = false,
-                can_be_pushed = false,
-                content_type,
-                content_length;
-            for(var x in headers){
-                if( headers[x].name == "Content-Type" && headers[x].value.indexOf("video") >= 0 ){
-                    is_video = true;
-                    content_type = headers[x].value;
-                }
-                if( headers[x].name == "Content-Length" && headers[x].value >1000000){
-                    can_be_pushed = true;
-                    content_length = headers[x].value;
-                }
-            }
-
-            if( is_video && can_be_pushed ){
-
-                videos[i]["content_type"] = content_type;
-                videos[i]["content_length"] = content_length;
-                videos[i]["video_url"] = details.url.split("?")[0];
-                i++;
-                console.log(videos,i);
-            }
-        }
-    },
-    {urls:["<all_urls>"]},
-    ["responseHeaders"]
-);
 
 
 var o = {
@@ -68,3 +11,90 @@ var o = {
     "name":"",
     "page_url":""
 }
+
+/*
+if( localStorage.length > 0 ){
+    for( var i=0; i < localStorage.length ; i++ ){
+        var key = localStorage.key(i);
+        videos[key] = localStorage.getItem(key);
+    }
+}
+*/
+
+// 视频信息
+var
+    // 视频计数
+    i = 0,
+
+    // 所加载页面是否为视频详情页
+    is_video_page = false;
+
+// 监测从content script传递来的信息
+chrome.extension.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        videos[i] = {};
+        videos[i]["name"] = request.name;
+        videos[i]["page_url"] = request.page_url;
+        is_video_page = true;
+        sendResponse("ok");
+    }
+);
+
+
+// detect received objects
+chrome.webRequest.onResponseStarted.addListener(
+    function(details){
+        //筛选符合条件的details
+        if( details.type == "object" && details.frameId == "0" && is_video_page){
+            var
+                // 返回的header信息
+                headers = details.responseHeaders,
+
+                // 是否为视频
+                is_video = false,
+
+                // 是否可加入videos
+                can_be_pushed = false,
+
+                // 文件类型
+                content_type,
+
+                // 文件大小
+                content_length;
+
+            // 根据返回header信息筛选合格视频
+            for(var x in headers){
+
+                // 判断是否为视频
+                if( headers[x].name == "Content-Type" && headers[x].value.indexOf("video") >= 0 ){
+                    is_video = true;
+                    content_type = headers[x].value;
+                }
+
+                // 根据视频大小判断是否为广告 (可能会失效)
+                if( headers[x].name == "Content-Length" && headers[x].value >1000000){
+                    can_be_pushed = true;
+                    content_length = headers[x].value;
+                }
+
+            }
+
+            //  将视频信息加入视频存储对象
+            if( is_video && can_be_pushed ){
+
+                videos[i]["content_type"] = content_type;
+                videos[i]["content_length"] = content_length;
+                videos[i]["video_url"] = details.url.split("?")[0];
+
+                // 为下一个视频做好准备
+                i++;
+
+                console.log(videos,i);
+            }
+        }
+    },
+    {urls:["<all_urls>"]},
+    ["responseHeaders"]
+);
+
+

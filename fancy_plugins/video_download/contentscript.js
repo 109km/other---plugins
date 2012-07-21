1481548d0b528ccf8be085303f632c2955a1b3dd
InMyWnd <shaoxinheng@gmail.com>
2012-07-21 21:18:19 +0800

    update

--- fancy_plugins/video_download/contentscript.js
+++ fancy_plugins/video_download/contentscript.js
@@ -1,31 +1,172 @@
 (function($){
+
+    var keyString = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+
+    var uTF8Encode = function(string) {
+        string = string.replace(/\x0d\x0a/g, "\x0a");
+        var output = "";
+        for (var n = 0; n < string.length; n++) {
+            var c = string.charCodeAt(n);
+            if (c < 128) {
+                output += String.fromCharCode(c);
+            } else if ((c > 127) && (c < 2048)) {
+                output += String.fromCharCode((c >> 6) | 192);
+                output += String.fromCharCode((c & 63) | 128);
+            } else {
+                output += String.fromCharCode((c >> 12) | 224);
+                output += String.fromCharCode(((c >> 6) & 63) | 128);
+                output += String.fromCharCode((c & 63) | 128);
+            }
+        }
+        return output;
+    };
+
+    var uTF8Decode = function(input) {
+        var string = "";
+        var i = 0;
+        var c = c1 = c2 = 0;
+        while ( i < input.length ) {
+            c = input.charCodeAt(i);
+            if (c < 128) {
+                string += String.fromCharCode(c);
+                i++;
+            } else if ((c > 191) && (c < 224)) {
+                c2 = input.charCodeAt(i+1);
+                string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+                i += 2;
+            } else {
+                c2 = input.charCodeAt(i+1);
+                c3 = input.charCodeAt(i+2);
+                string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+                i += 3;
+            }
+        }
+        return string;
+    }
+
+    $.extend({
+        base64Encode: function(input) {
+            var output = "";
+            var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+            var i = 0;
+            input = uTF8Encode(input);
+            while (i < input.length) {
+                chr1 = input.charCodeAt(i++);
+                chr2 = input.charCodeAt(i++);
+                chr3 = input.charCodeAt(i++);
+                enc1 = chr1 >> 2;
+                enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+                enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+                enc4 = chr3 & 63;
+                if (isNaN(chr2)) {
+                    enc3 = enc4 = 64;
+                } else if (isNaN(chr3)) {
+                    enc4 = 64;
+                }
+                output = output + keyString.charAt(enc1) + keyString.charAt(enc2) + keyString.charAt(enc3) + keyString.charAt(enc4);
+            }
+            return output;
+        },
+        base64Decode: function(input) {
+            var output = "";
+            var chr1, chr2, chr3;
+            var enc1, enc2, enc3, enc4;
+            var i = 0;
+            input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+            while (i < input.length) {
+                enc1 = keyString.indexOf(input.charAt(i++));
+                enc2 = keyString.indexOf(input.charAt(i++));
+                enc3 = keyString.indexOf(input.charAt(i++));
+                enc4 = keyString.indexOf(input.charAt(i++));
+                chr1 = (enc1 << 2) | (enc2 >> 4);
+                chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+                chr3 = ((enc3 & 3) << 6) | enc4;
+                output = output + String.fromCharCode(chr1);
+                if (enc3 != 64) {
+                    output = output + String.fromCharCode(chr2);
+                }
+                if (enc4 != 64) {
+                    output = output + String.fromCharCode(chr3);
+                }
+            }
+            output = uTF8Decode(output);
+            return output;
+        }
+    });
+})(jQuery);
+
+
+
+(function($){
     var wanDouJiaExt = {
         init: function(){
+            if( wanDouJiaExt.page_url == "http://www.youku.com/" ){
+                wanDouJiaExt.modifyHomePage();
+            }
+
+            if( wanDouJiaExt.page_url.indexOf("tv.youku.com")>= 0 ){
+                wanDouJiaExt.modifyTvPage();
+            }
+
             wanDouJiaExt.modifyDetailPage();
             wanDouJiaExt.clearAds();
-            //wanDouJiaExt.removeTarget();
+            wanDouJiaExt.removeTarget();
+            wanDouJiaExt.removeTag();
+        },
+        page_url : location.href,
+        base_down_url:"http://videodl.sinaapp.com/?url=",
+
+        modifyHomePage:function(){
+            $("body .window .collfocus .collappend .items").find(".clear").hide();
+        },
+        modifyTvPage:function(){
+            $(".collgrid4w .items").find(".clear").hide();
         },
         modifyDetailPage:function(){
 
             // disable ads
             var video_layer = $('<div class="video_layer"></div>'),
+                btn_layer =$('<div class="video_btn_layer"></div>'),
                 player = $("#player"),
                 width = player.width(),
                 height = player.height()-100;
             player.find("object").append('<param name="wmode" value="transparent">');
             video_layer.height(height).width(width);
-            player.append(video_layer);
+            player.append(video_layer).append(btn_layer);
 
+            // remove title link
+            $("#vpvideotitle a").removeAttr("href");
 
+            $("#vpofficialtitle a").length>0 && $("#vpofficialtitle a").removeAttr("href");
+
+            // add download btn
+            var down_url = wanDouJiaExt.base_down_url + $.base64Encode(wanDouJiaExt.page_url),
+                name = $.trim(document.title.slice(0,document.title.indexOf("-")));
+
+            down_url = down_url + "#name=" + name + "&content-type=video/mp4";
+            var down_btn = $('<a class="download_btn" href="'+down_url+'" rel="download">下载视频</a>');
+            $(".show_intro .title").append(down_btn);
+        },
+        modifyPlayList:function(){
 
         },
         clearAds:function(){
-            $('div','body').each(function(){
-                var self = $(this);
-                if ( self.attr('id').indexOf("ab_") >= 0 ){
-                    self.css("display","none!important");
+            setInterval(function(){
+                var count=0;
+                $('div','body').each(function(){
+                    var self = $(this);
+                    if ( self.attr('id').indexOf("ab_") >= 0 ){
+                        self.css("display","none");
+                        count++;
+                    }
+                });
+
+                if (count == 0){
+                    wanDouJiaExt.is_check_ads = false;
                 }
-            });
+
+            },2000);
+
         },
         // get params from a url
         request_url:function (url){
@@ -41,8 +182,20 @@
             }
             return request;
         },
+        removeTag:function(){
+
+            $('.sooption .options li').each(function(i){
+                if(i > 1)
+                {
+                    $(this).remove();
+                }
+            });
+            //$('.rank10coll .clear').remove();
+        },
         removeTarget:function(){
-            $('a','body').removeAttr('target');
+            setTimeout(function(){
+                $('a','body').removeAttr('target');
+            },1000);
         }
     };
     wanDouJiaExt.init();
--- fix/qyer/contentscript.js
+++ fix/qyer/contentscript.js
@@ -3,53 +3,24 @@
 (function($){
     var wanDouJiaExt = {
         init: function(){
-            wanDouJiaExt.autoLogin();
             if(location.href.indexOf("index_action")>= 0 ){
                 wanDouJiaExt.modifyDetailPage();
             }
-
             wanDouJiaExt.removeTarget();
-        },
-        autoLogin:function(){
-
-            // 从guide跳到login
-            if( $("#asynclogininfo .qyer_head_login_entry").length > 0 && $("#asynclogininfo .qyer_head_login_entry a").length == 3 && location.href.indexOf("guide") >= 0 ){
-                $("body").hide();
-                location.href = "http://login.qyer.com/login.php";
-            }
-
-            // 执行登录
-            if( $("#loginform").length > 0 ){
-                $("body").hide();
-                $("#account").val("wandoujia_qyer");
-                $(".infoform_txt[type='password']").val("wandoujia163");
-                $(".infoform_btn input").val("aHR0cDovL2d1aWRlLnF5ZXIuY29tLw==");
-                $("#loginbtn").trigger("click");
-            }
-
         },
         modifyDetailPage:function(){
             $(".jn_cover a,.jn_author_info a").removeAttr("href");
 
-            var href = location.href,
-                id = href.slice(href.indexOf("id_")+3,href.length),
+            var
                 name = $(".gui_banner_title_cn").text(),
-                down_url = "http://guide.qyer.com/index_action_downguide_id_"+id+"_isdown_1" + "#name=" +
+                down_url = "http://tg-con.wandoujia.com:29421/qiongyou/"+name+".pdf" + "#name=" +
                     name + "&content-type=application/pdf",
                 file_name = name + ".pdf",
                 down_btn ;
 
-            var cdb_auth = wanDouJiaExt.cookie("cdb_auth").toString();
-
-            wanDouJiaExt.cookie("cdb_auth",cdb_auth,{domain:"qyer.com"});
-
-            console.log(document.cookie);
             down_btn = $('<p class="down_btn"><a href="'+down_url+'" download="'+file_name+'">点此下载</a></p>');
 
             $(".gui_banner_title").append(down_btn);
-            down_btn.click(function(){
-                location.href = down_url;
-            });
         },
         // get params from a url
         request_url:function (url){
@@ -69,44 +40,6 @@
             setTimeout(function(){
                 $('a','body').removeAttr('target');
             },1000);
-        },
-        cookie:function(name, value, options) {
-            if (typeof value != 'undefined') {
-                options = options || {};
-                if (value === null) {
-                    value = '';
-                    options = $.extend({}, options);
-                    options.expires = -1;
-                }
-                var expires = '';
-                if (options.expires && (typeof options.expires == 'number' || options.expires.toUTCString)) {
-                    var date;
-                    if (typeof options.expires == 'number') {
-                        date = new Date();
-                        date.setTime(date.getTime() + (options.expires * 24 * 60 * 60 * 1000));
-                    } else {
-                        date = options.expires;
-                    }
-                    expires = '; expires=' + date.toUTCString();
-                }
-                var path = options.path ? '; path=' + (options.path) : '';
-                var domain = options.domain ? '; domain=' + (options.domain) : '';
-                var secure = options.secure ? '; secure' : '';
-                document.cookie = [name, '=', encodeURIComponent(value), expires, path, domain, secure].join('');
-            } else {
-                var cookieValue = null;
-                if (document.cookie && document.cookie != '') {
-                    var cookies = document.cookie.split(';');
-                    for (var i = 0; i < cookies.length; i++) {
-                        var cookie = jQuery.trim(cookies[i]);
-                        if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                            cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                            break;
-                        }
-                    }
-                }
-                return cookieValue;
-            }
         }
     };
     wanDouJiaExt.init();

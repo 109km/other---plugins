de07d0030041750c77f7716fddcd7e6463f2f19b
InMyWnd <shaoxinheng@gmail.com>
2012-12-06 02:13:02 +0800

    update

--- 201212/91手机铃声/contentscript.js
+++ 201212/91手机铃声/contentscript.js
@@ -1,50 +1,51 @@
 (function($){
     var wanDouJiaExt = {
         init: function(){
-            var url = location.href;
             wanDouJiaExt.modifyGlobalPage();
         },
         modifyGlobalPage:function(){
+            var url = location.href;
+            if ( url.indexOf("Sort") < 0 ){
 
-            var items1 = $(".ring_list1 li");
-            if ( items1 ){
-                items1.each(function(){
-                    var self = $(this),
-                        name = $.trim(self.find("h3 a").text()),
-                        link = self.find(".link1").attr("href"),
-                        start = link.indexOf("(") + 2,
-                        end = link.indexOf("mp3",start)+3,
-                        mp3_link = link.slice(start,end);
+                var items1 = $(".ring_list1 li");
+                if ( items1 ){
+                    items1.each(function(){
+                        var self = $(this),
+                            name = $.trim(self.find("h3 a").text()),
+                            link = self.find(".link1").attr("href"),
+                            start = link.indexOf("(") + 2,
+                            end = link.indexOf("mp3",start)+3,
+                            mp3_link = link.slice(start,end);
 
-                    mp3_link = mp3_link + "#name=" + name + "&content-type=audio";
+                        mp3_link = mp3_link + "#name=" + name + "&content-type=audio";
 
-                    self.find(".link3").attr("href",mp3_link).attr("download","");
+                        self.find(".link3").attr("href",mp3_link).attr("download","");
 
-                });
-            }
+                    });
+                }
 
 
-            var items2 = $(".ring_list2 tbody tr");
-            if ( items2 ){
-                items2.each(function(){
-                    var self = $(this),
-                        name = $.trim(self.find(".col1 a").text()),
-                        link = self.find(".col1 a").attr("href"),
-                        start = link.indexOf("(") + 2,
-                        end = link.indexOf("mp3",start)+3,
-                        mp3_link = link.slice(start,end);
+                var items2 = $(".ring_list2 tbody tr");
+                if ( items2 ){
+                    items2.each(function(){
+                        var self = $(this),
+                            name = $.trim(self.find(".col1 a").text()),
+                            link = self.find(".col1 a").attr("href"),
+                            start = link.indexOf("(") + 2,
+                            end = link.indexOf("mp3",start)+3,
+                            mp3_link = link.slice(start,end);
 
-                    mp3_link = mp3_link + "#name=" + name + "&content-type=audio";
+                        mp3_link = mp3_link + "#name=" + name + "&content-type=audio";
 
-                    self.find(".link3").attr("href",mp3_link).attr("download","");
+                        self.find(".link3").attr("href",mp3_link).attr("download","");
 
-                });
-            }
-            if ( $(".sort_tabCon") ){
-                $(".sort_tabCon").first().show();
-            }
-            var items3 = $(".sort_con_con .sort_list1_con tr");
+                    });
+                }
+            }else{
+            var items3 = $(".sort_tabCon").first().find(".sort_con_con .sort_list1_con tr");
             if ( items3 ){
+                $("#sort_tab li").last().hide();
+                $(".sort_tabCon").first().show().css("position","static");
                 items3.each(function(){
                     var self = $(this),
                         name = $.trim(self.find(".col2 a").text()),
@@ -61,11 +62,16 @@
                             mp3_link = "http://ringdown.91.2366.com" + decodeURIComponent(src);
                             mp3_link = mp3_link + "#name=" + name + "&content-type=audio";
                             self.find(".col2 a").attr("href",mp3_link).attr("download","");
+                        },
+                        error:function(){
+                            console.log("error");
                         }
                     });
 
 
                 });
+
+                }
             }
 
 
